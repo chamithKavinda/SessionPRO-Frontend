de20d4960c47dbebcebbbc3fe7c63ea5c6bb5c8d
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2025-02-20 11:51:16 +0530

    fix: mySession card three dot issue

--- src/components/SessionCard.tsx
+++ src/components/SessionCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { toast } from "react-toastify";
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../store/store';
-import { registerSession} from '../reducer/mySession-reducer';
+import { registerSession } from '../reducer/mySession-reducer';
 import Session from '../models/session';
 
 interface SessionCardProps {
@@ -37,11 +37,13 @@ const SessionCard: React.FC<SessionCardProps> = ({ session, isRegistered, handle
       className="w-[250px] h-[295px] mx-auto hover:animate-background rounded-xl shadow-2xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]"
     >
       <div className="relative">
-        <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-800" onClick={() => handleOptionsClick(session.sessionID)}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12 12.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12 18.75a.75.75 0 100-1.5.75.75 0 000 1.5z" />
-          </svg>
-        </button>
+        {!isRegistered && (
+          <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-800" onClick={() => handleOptionsClick(session.sessionID)}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12 12.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12 18.75a.75.75 0 100-1.5.75.75 0 000 1.5z" />
+            </svg>
+          </button>
+        )}
 
         {selectedSessionId === session.sessionID && (
           <div className="absolute top-0 right-0 mt-8 w-32 bg-gray-100 shadow-lg rounded-lg p-2">
@@ -62,7 +64,7 @@ const SessionCard: React.FC<SessionCardProps> = ({ session, isRegistered, handle
 
         {isRegistered ? (
           <button
-            className="mt-4 w-full bg-red-600 text-white h-9 rounded-xl hover:bg-red-500 transition"
+            className="mt-4 w-full bg-red-500 text-white h-9 rounded-xl hover:bg-red-500 transition"
             onClick={handleRemove}
           >
             Remove
